a6434de8940611c6b10a91735a822be5b2e461f2
evelyn_zhang <1054982653@qq.com>
2017-01-24 17:24:04 +0800

    match page

--- src/dist/all.js
+++ src/dist/all.js
@@ -756,22 +756,6 @@ $(document).ready(function(){
 
         });
 
-        var mySwiper = new Swiper ('.swiper-container', {
-            // Optional parameters
-            //direction: 'horizen',
-            loop: true,
-
-            // If we need pagination
-            //pagination: '.swiper-pagination',
-
-            // Navigation arrows
-            nextButton: '.swiper-button-next',
-            prevButton: '.swiper-button-prev',
-
-            // And if we need scrollbar
-            //scrollbar: '.swiper-scrollbar',
-        })
-
 
     };
     //validate the input is a-z or A-Z
