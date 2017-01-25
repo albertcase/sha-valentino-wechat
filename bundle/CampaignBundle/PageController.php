7e9e0d98c4772b668501ba5c9b0bd020d75e22ce
Demon Zhang <demon.zhang@samesamechina.com>
2017-01-25 15:21:37 +0800

    no message

--- bundle/CampaignBundle/PageController.php
+++ bundle/CampaignBundle/PageController.php
@@ -9,6 +9,19 @@ class PageController extends Controller {
 		$this->render('index');
 	}
 
+	public function matchAction() {	
+		$request = $this->request;
+		$fields = array(
+			'id' => array('notnull', '120'),
+		);
+		$request->validation($fields);
+		$id = $request->query->get('id');
+		$databaseAPI = new \Lib\DatabaseAPI();
+		$rs = $databaseAPI->loadMakeById($id);
+		var_dump($rs);exit;
+		$this->render('index');
+	}
+
 	public function clearCookieAction() {
 		setcookie('_user', json_encode($user), time(), '/');
 		$this->statusPrint('success');
