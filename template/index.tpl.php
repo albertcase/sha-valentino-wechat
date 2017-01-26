bcc5248593dd4cbbd7e74e3ecf4e697a22f5aa20
evelyn_zhang <1054982653@qq.com>
2017-01-26 19:47:14 +0800

    index

--- template/index.tpl.php
+++ template/index.tpl.php
@@ -1,68 +1,149 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>xxxx</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<!--禁用手机号码链接(for iPhone)-->
-	<meta name="format-detection" content="telephone=no">
-
-	<!--自适应设备宽度-->
-	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimum-scale=1.0,maximum-scale=1.0,minimal-ui" />
-	
-	<!--控制全屏时顶部状态栏的外，默认白色-->
-	<meta name="apple-mobile-web-app-status-bar-style" content="black">
-
-	<!--是否启动webapp功能，会删除默认的苹果工具栏和菜单栏。-->
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<meta name="Keywords" content="">
-	<meta name="Description" content="...">
-
-	<link rel='stylesheet' type='text/css' href="/src/css/reset.css">
-	<link rel='stylesheet' type='text/css' href="/src/css/common.css">
-	<link rel='stylesheet' type='text/css' href="/src/css/main.css">
-	<script>
-
-	</script>
-	<script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
-	<script type="text/javascript" src="src/js/jquery.js"></script>
-	<script type="text/javascript" src="src/js/rem.js"></script>
-	<script type="text/javascript" src="src/js/main.js"></script>
-	<script type="text/javascript" src="src/js/share.js"></script>
+<head lang="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>RockStud Guitar Strap</title>
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="x5-fullscreen" content="true">
+    <meta name="full-screen" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+    <link rel="stylesheet" type="text/css" href="/src/dist/css/style.css" />
+    <script type="text/javascript" src="http://valentinowechat.samesamechina.com/api/v1/js/80c73ea5-de82-4aff-98ff-fd79aad6041d/wechat?debug=1"></script>
+    <script src="/src/assets/js/lib/zepto.min.js"></script>
+    <script src="/src/assets/js/lib/pre-loader.js"></script>
+    <script src="/src/assets/js/lib/reqAnimate.js"></script>
+    <script src="/src/assets/js/rem.js"></script>
+    <script src="/src/assets/js/common.js"></script>
+    <script src="/src/assets/js/api.js"></script>
+    <script src="/src/assets/js/wxshare.js"></script>
+    <script src="/src/assets/js/home.js"></script>
 </head>
-<body>
-
-<div class="wrapper">
-
-</div>
-
-<!-- 横屏代码 -->
+<body class="page-home">
 <div id="orientLayer" class="mod-orient-layer">
     <div class="mod-orient-layer__content">
         <i class="icon mod-orient-layer__icon-orient"></i>
-        <div class="mod-orient-layer__desc">为了更好的体验，请使用竖屏浏览</div>
+        <div class="mod-orient-layer__desc">请在解锁模式下使用竖屏浏览</div>
     </div>
 </div>
+<!--main content-->
+<div class="wrapper">
+    <div class="container">
+        <!-- landing page-->
+        <div class="pin pin-1 current" id="landing-page">
+            <div class="layer-bg">
+                <div class="lbg bg-1">
+                    <div class="color-area orange"></div>
+                    <div class="pipe">
+                        <img src="/src/dist/images/pipe-2.png" alt=""/>
+                    </div>
+                </div>
+                <div class="lbg bg-2">
+                    <div class="color-area blue"></div>
+                    <div class="pipe">
+                        <img src="/src/dist/images/pipe-1.png" alt=""/>
+                    </div>
+                </div>
+            </div>
+            <div class="content">
+                <div class="logo">
+                    <img src="/src/dist/images/logo.png" alt="valentino"/>
+                </div>
+                <div class="p1-t1">
+                    <img src="/src/dist/images/p1-t1.png" alt="精致刺绣，魅惑无限"/>
+                </div>
+                <div class="video-block">
+                    <video src="/src/dist/video/test.mp4"  poster="/src/dist/images/poster.jpg">
+                        Sorry, your browser doesn't support embedded videos
+                    </video>
+                </div>
+                <div class="btn btn-start-custom">
+                    <div class="icon">
+                        <div class="circle"></div>
+                        <div class="dot"></div>
+                    </div>
+                    <div class="text">
+                        <img src="/src/dist/images/btn-text-1.png" alt="valentino"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- select straps style, then custom the words with two-->
+        <div class="pin pin-2" id="select-page">
+            <div class="switch-content">
+                <div class="show-img">
+                    <img src="/src/dist/images/straps-1.png" alt="valentino"/>
+                </div>
+                <!--whiteandblack-->
+                <!--<div class="show-word">-->
+                    <!--<span class="letter letter-a" id="first-letter"></span>-->
+                    <!--<span class="letter letter-b" id="second-letter"></span>-->
+                <!--</div>-->
+                <div class="show-word">
+                    <span class="sw-1 letter letter-a"></span>
+                    <span class="sw-2 dot"></span>
+                    <span class="sw-3 letter letter-b"></span>
+                    <span class="sw-4 dot"></span>
+                </div>
+            </div>
+            <div class="switch-text">
+                <div class="p2-t1">
+                    <img src="/src/dist/images/p2-text-1.png" alt=""/>
+                </div>
+                <div class="steps">
+                    <div class="num">1/2</div>
+                    <!--<div class="control">next</div>-->
+                </div>
+            </div>
+            <div class="switch-menu">
+                <div class="step step-1 current">
+                    <h3 class="tips">图案</h3>
+                    <ul class="lists">
+                        <li class="item item-1 active"></li>
+                        <li class="item item-2"></li>
+                        <li class="item item-3"></li>
+                    </ul>
+                    <div class="control">确定</div>
+                </div>
+                <div class="step step-2">
+                    <h3 class="tips">专属字母</h3>
+                    <div class="input-box">
+                        <ul class="lists">
+                            <li class="item item-1 active"></li>
+                            <li class="item item-2"></li>
+                        </ul>
+                        <input type="text" class="input-custom" maxlength="2" placeholder="请在此输入两个字母"/>
+                        <div class="btn btn-finish control">完成</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pin pin-3 pin-result" id="doneshare-page">
+            <!--<div class="guide-share">-->
+                <!--<img src="/src/dist/images/guide-share.png" alt=""/>-->
+            <!--</div>-->
+            <div class="p3-t1">
+                <img src="/src/dist/images/p3-t1.png" alt=""/>
+            </div>
+            <div class="generate-show">
+                <div class="show-animate">
+                    <img src="/src/dist/images/straps1/straps1_00000.jpg" alt=""/>
+                </div>
+                <!--whiteandblack-->
+                <div class="show-word">
+                    <span class="letter letter-a" id="first-letter-1"></span>
+                    <span class="letter letter-b" id="second-letter-2"></span>
+                </div>
+            </div>
+            <div class="p3-t2">
+                <img src="/src/dist/images/p3-t2.png" alt=""/>
+            </div>
+            <div class="btn btn-goreservation">
+                <a href="reservation.html">开始预约</a>
+            </div>
+        </div>
 
-<script>
-wechatShareConfig(<?php print $config; ?>);
-
-wx.ready(function(){
-	wechatShare({
-			title: 'xxxxxx',
-			t_title: 'xxxxxxx',
-			link: location.href,
-			img: "http://" + window.location.host + "/src/img/share.jpg",
-			desc: 'xxxxxxx'
-		}, function(env){
-			//callback fun
-		});
-
-	// wx.hideMenuItems({
-		//   		menuList: data.exclude // 要隐藏的菜单项，只能隐藏“传播类”和“保护类”按钮，所有menu项见附录3
-	// });
-
-});
-
-</script>
+    </div>
+</div>
 </body>
 </html>
